dbfb4ad5da4531a5d847d24e74d8be99a52b163a
Frank Febbraro <frank@phase2technology.com>
2017-07-25 14:44:23 -0700

    Reformatted repo to version folders

--- build.sh
+++ build.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-docker build -t outrigger/mariadb:10.1 .
+set -e
+
+# Get the versions to build
+
+# If they are passed as args
+if [ $# -gt 0 ]; then
+  buildDirs="$@"
+else
+  # else, get all the relevant dirs
+  buildDirs=$(ls | grep -E '^[0-9\.]+$')
+fi
+
+# Build each one of them
+for version in $buildDirs; do
+  pushd $version
+  docker build -t outrigger/mariadb:${version} .
+  popd
+done
